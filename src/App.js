8c0579c9b0a60ad6907ab53e220d411d19751a4e
NickLThompson <thompsonnickolas1@gmail.com>
2022-09-15 16:54:53 -0400

    added the ability to scroll and properly centered the items. learning a lot

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './style.css';
 
 function App() {
+  const [count] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section>
+        <h1>Welcome to my website!</h1>
+        <p>this is where all my work stuff will be shown!</p>
+      </section>
+
+      <section>
+        <h2>This is the 2nd part</h2>
+        <p>not sure what to put here</p>
+      </section>
+
+      <section>
+        <h3>alright just one more thing</h3>
+        <p>filler text here honestly</p>
+      </section>
+      
+    </main>
   );
 }
 
