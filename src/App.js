00fd3243c48512eb5ab7e122cfbecd1bd1d2dd43
NickLThompson <thompsonnickolas1@gmail.com>
2022-09-16 14:08:09 -0400

    learning bootstrap styling and putting a navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
-import './style.css';
+import "./components/navbar"
+import './styles/style.css';
 
 function App() {
-  const [count] = useState(0);
+  // useState();
 
   return (
     <main>
-      <section>
+      <section className="section1">
         <h1>Welcome to my website!</h1>
         <p>this is where all my work stuff will be shown!</p>
       </section>
 
-      <section>
+      <section className="section2">
         <h2>This is the 2nd part</h2>
         <p>not sure what to put here</p>
       </section>
 
-      <section>
+      <section className="section3">
         <h3>alright just one more thing</h3>
         <p>filler text here honestly</p>
       </section>
---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav class="navbar">
+            <ul>Home</ul>
+            <ul>About</ul>
+            <ul>Work</ul>
+            <ul>Contact</ul>
+            <ul>Resume</ul>
+        </nav>
+
+    )
+}
+
+export default Navbar;
