936b89ef8632860599615aff9b760797d6ab5d2b
NickLThompson <thompsonnickolas1@gmail.com>
2022-09-16 17:41:12 -0400

    finally got the header+navbar showing up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./components/navbar"
 import './styles/style.css';
 
 function App() {
@@ -7,20 +6,30 @@ function App() {
 
   return (
     <main>
-      <section className="section1">
+      <header className="header">
+        <nav className="navbar">
+          <ul>test</ul>
+          <ul>test 2</ul>
+          <ul>test 3</ul>
+          <ul>test 4</ul>
+        </nav>
+      </header>
+      
+      <section className="section">
         <h1>Welcome to my website!</h1>
         <p>this is where all my work stuff will be shown!</p>
       </section>
 
-      <section className="section2">
+      <section className="section">
         <h2>This is the 2nd part</h2>
         <p>not sure what to put here</p>
       </section>
 
-      <section className="section3">
+      <section className="section">
         <h3>alright just one more thing</h3>
         <p>filler text here honestly</p>
       </section>
+
       
     </main>
   );
