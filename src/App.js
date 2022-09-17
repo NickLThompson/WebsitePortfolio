68359ab11605a1d0f676cb89bd0b527ac6b3d719
NickLThompson <thompsonnickolas1@gmail.com>
2022-09-17 00:59:03 -0400

    fixed navbar and all the content in it

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
 
   return (
     <main>
-      <Navbar bg="primary" variant="dark">
-        <Container>
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#home">About</Nav.Link>
-          <Nav.Link href="#home">Portfolio</Nav.Link>
-          <Nav.Link href="#home">Contact Me</Nav.Link>
-          <Nav.Link href="#home">Resume</Nav.Link>
+      <Navbar bg="dark" variant="dark" sticky="top">
+        <Container className="navbar">
+          <Nav.Link className="navlink" href="#home">Home</Nav.Link>
+          <Nav.Link className="navlink" href="#about">About</Nav.Link>
+          <Nav.Link className="navlink" href="#portfolio">Portfolio</Nav.Link>
+          <Nav.Link className="navlink" href="#contactMe">Contact Me</Nav.Link>
+          <Nav.Link className="navlink" href="#resume">Resume</Nav.Link>
         </Container>
       </Navbar>
 
