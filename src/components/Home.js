5a376817efece0071ca11d48fe09b5f54f6118c2
NickLThompson <thompsonnickolas1@gmail.com>
2022-09-27 17:38:09 -0400

    added id name home to the section

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <section className='container'>
+        <section className='section' id="home">
             <div class="row">
-                <div className="col-8 text-center" id="starter">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+                <div className="col-6 text-center" id="starter">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
                 <div className="col text-center">
                     <img
                         src={require("../images/PortfolioImage1.png")}
@@ -17,11 +17,4 @@ const Home = () => {
         </section>
     )
 }
-
 export default Home
-
-//             <img
-//                 src={require("../images/PortfolioImage1.png")}
-//                 alt="Portfolio Image"
-//                 id="portfolioImage">
-//             </img>
